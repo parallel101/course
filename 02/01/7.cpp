84e8126e18271e5db7f895e90945c3fd8ee37d17
archibate <1931127624@qq.com>
2021-12-18 23:46:19 +0800

    fin

--- 02/01/7.cpp
+++ 02/01/7.cpp
@@ -5,8 +5,9 @@
 int main() {
     std::vector v = {4, 3, 2, 1};
 
-    int sum = std::reduce(v.begin(), v.end());
-    // int sum = std::reduce(v.begin(), v.end(), 0, std::plus{});
+    int sum = std::reduce(v.begin(), v.end(), 0, [] (int x, int y) {
+        return x + y;
+    });
 
     std::cout << sum << std::endl;
     return 0;
