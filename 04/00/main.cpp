cdc1a23fd9fdcceab32042feb525e0da3a70bba6
archibate <1931127624@qq.com>
2021-12-26 14:35:25 +0800

    better folding

--- 04/00/main.cpp
+++ 04/00/main.cpp
@@ -1,5 +1,5 @@
-void func(int *a, int n) {
-    for (int i = 0; i < n; i++) {
-        a[i] = i;
+void func(float *a, float *b) {
+    for (int i = 0; i < 1024; i++) {
+        a[i] = a[i] + 1;
     }
 }
