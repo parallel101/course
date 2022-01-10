75d181947bb600517dd4aa39dea739139b4b4e67
archibate <1931127624@qq.com>
2022-01-10 21:43:30 +0800

    use pod

--- 06/05_filter/08/main.cpp
+++ 06/05_filter/08/main.cpp
@@ -4,25 +4,27 @@
 #include <cmath>
 #include <atomic>
 #include "ticktock.h"
+#include "pod.h"
 
 int main() {
     size_t n = 1<<27;
-    std::vector<float> a(n);
+    std::vector<pod<float>> a;
     std::atomic<size_t> a_size = 0;
 
     TICK(filter);
+    a.resize(n);
     tbb::parallel_for(tbb::blocked_range<size_t>(0, n),
     [&] (tbb::blocked_range<size_t> r) {
-        std::vector<float> local_a;
-        local_a.reserve(r.size());
+        std::vector<pod<float>> local_a(r.size());
+        size_t lasize = 0;
         for (size_t i = r.begin(); i < r.end(); i++) {
             float val = std::sin(i);
             if (val > 0) {
-                local_a.push_back(val);
+                local_a[lasize++] = val;
             }
         }
-        size_t base = a_size.fetch_add(local_a.size());
-        for (size_t i = 0; i < local_a.size(); i++) {
+        size_t base = a_size.fetch_add(lasize);
+        for (size_t i = 0; i < lasize; i++) {
             a[base + i] = local_a[i];
         }
     });
