f34df364985601d20691a66c23b34190c573fdd7
archibate <1931127624@qq.com>
2022-01-22 01:15:45 +0800

    add more blocking

--- 07/07_morton/02/main.cpp
+++ 07/07_morton/02/main.cpp
@@ -9,75 +9,60 @@
 #include <omp.h>
 #include "ndarray.h"
 #include "morton.h"
-#ifdef WITH_TBB
-#include <tbb/parallel_for.h>
-#include <tbb/blocked_range2d.h>
-#endif
 
 // L1: 32KB
 // L2: 256KB
 // L3: 12MB
 
 constexpr int n = 1<<10;
-constexpr int nblur = 8;
 
-ndarray<2, float, 16> a(n, n);
+ndarray<2, float> a(n, n);
 ndarray<2, float> b(n, n);
+ndarray<2, float> c(n, n);
 
-void BM_y_blur(benchmark::State &bm) {
+void BM_matmul(benchmark::State &bm) {
     for (auto _: bm) {
-#pragma omp parallel for collapse(2)
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < n; x++) {
-                for (int t = 0; t < nblur; t++) {
-                    b(x, y) += a(x, y + t);
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n; i++) {
+                for (int t = 0; t < n; t++) {
+                    a(i, j) += b(i, t) * c(t, j);
                 }
             }
         }
-        benchmark::DoNotOptimize(b);
     }
 }
-BENCHMARK(BM_y_blur);
+BENCHMARK(BM_matmul);
 
-void BM_y_blur_tiled(benchmark::State &bm) {
+void BM_matmul_blocked(benchmark::State &bm) {
     for (auto _: bm) {
-        constexpr int blockSize = 32;
-#pragma omp parallel for collapse(2)
-        for (int yBase = 0; yBase < n; yBase += blockSize) {
-            for (int xBase = 0; xBase < n; xBase += blockSize) {
-                for (int y = yBase; y < yBase + blockSize; y++) {
-                    for (int x = xBase; x < xBase + blockSize; x++) {
-                        for (int t = -nblur; t <= nblur; t++) {
-                            b(x, y) += a(x, y + t);
+        for (int j = 0; j < n; j++) {
+            for (int iBase = 0; iBase < n; iBase += 32) {
+                for (int t = 0; t < n; t++) {
+                    for (int i = iBase; i < iBase + 32; i++) {
+                        a(i, j) += b(i, t) * c(t, j);
+                    }
+                }
+            }
+        }
+    }
+}
+BENCHMARK(BM_matmul_blocked);
+
+void BM_matmul_blocked_both(benchmark::State &bm) {
+    for (auto _: bm) {
+        for (int jBase = 0; jBase < n; jBase += 16) {
+            for (int iBase = 0; iBase < n; iBase += 16) {
+                for (int j = jBase; j < jBase + 16; j++) {
+                    for (int t = 0; t < n; t++) {
+                        for (int i = iBase; i < iBase + 16; i++) {
+                            a(i, j) += b(i, t) * c(t, j);
                         }
                     }
                 }
             }
         }
-        benchmark::DoNotOptimize(b);
     }
 }
-BENCHMARK(BM_y_blur_tiled);
-
-void BM_y_blur_morton_tiled(benchmark::State &bm) {
-    for (auto _: bm) {
-        constexpr int blockSize = 32;
-#pragma omp parallel for
-        for (int mortonCode = 0; mortonCode < n * n / blockSize / blockSize; mortonCode++) {
-            auto [xBase, yBase] = morton2d::decode(mortonCode);
-            xBase *= blockSize;
-            yBase *= blockSize;
-            for (int y = yBase; y < yBase + blockSize; y++) {
-                for (int x = xBase; x < xBase + blockSize; x++) {
-                    for (int t = -nblur; t <= nblur; t++) {
-                        b(x, y) += a(x, y + t);
-                    }
-                }
-            }
-        }
-        benchmark::DoNotOptimize(b);
-    }
-}
-BENCHMARK(BM_y_blur_morton_tiled);
+BENCHMARK(BM_matmul_blocked_both);
 
 BENCHMARK_MAIN();
