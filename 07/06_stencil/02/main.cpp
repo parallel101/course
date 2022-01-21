6cd1721665c6ae7925ca41db93f849f5560b7a7d
archibate <1931127624@qq.com>
2022-01-21 11:41:43 +0800

    gs

--- 07/06_stencil/02/main.cpp
+++ 07/06_stencil/02/main.cpp
@@ -299,4 +299,108 @@ void BM_y_blur_tiled_only_x_prefetched_streamed_merged_vectorized_interchanged_u
 }
 BENCHMARK(BM_y_blur_tiled_only_x_prefetched_streamed_merged_vectorized_interchanged_unrolled_avx);
 
+void BM_y_blur_tiled_only_x_prefetched_streamed_merged_vectorized_interchanged_unrolled_avx_forwarded(benchmark::State &bm) {
+    for (auto _: bm) {
+#pragma omp parallel for collapse(2)
+        for (int x = 0; x < nx; x += 32) {
+            for (int y = 0; y < ny; y++) {
+                _mm_prefetch(&a(x, y + nblur + 40), _MM_HINT_T0);
+                _mm_prefetch(&a(x + 16, y + nblur + 40), _MM_HINT_T0);
+                __m256 res[4];
+#pragma GCC unroll 4
+                for (int offset = 0; offset < 4; offset++) {
+                    res[offset] = _mm256_setzero_ps();
+                }
+                for (int t = -nblur; t <= nblur; t++) {
+#pragma GCC unroll 4
+                    for (int offset = 0; offset < 4; offset++) {
+                        res[offset] = _mm256_add_ps(res[offset],
+                            _mm256_load_ps(&a(x + offset * 8, y + t)));
+                    }
+                }
+#pragma GCC unroll 4
+                for (int offset = 0; offset < 4; offset++) {
+                    _mm256_stream_ps(&b(x + offset * 8, y), res[offset]);
+                }
+            }
+        }
+        benchmark::DoNotOptimize(a);
+    }
+}
+BENCHMARK(BM_y_blur_tiled_only_x_prefetched_streamed_merged_vectorized_interchanged_unrolled_avx_forwarded);
+
+#if 0
+void BM_y_blur_tiled_only_x_prefetched_streamed_merged_vectorized_interchanged_unrolled_avx_forwarded_manually(benchmark::State &bm) {
+    for (auto _: bm) {
+#pragma omp parallel for collapse(2)
+        for (int x = 0; x < nx; x += 32) {
+            for (int y = 0; y < ny; y++) {
+                _mm_prefetch(&a(x, y + nblur + 40), _MM_HINT_T0);
+                _mm_prefetch(&a(x + 16, y + nblur + 40), _MM_HINT_T0);
+                __m256 res0 = _mm256_load_ps(&a(x + 0, y - nblur));
+                __m256 res1 = _mm256_load_ps(&a(x + 8, y - nblur));
+                __m256 res2 = _mm256_load_ps(&a(x + 16, y - nblur));
+                __m256 res3 = _mm256_load_ps(&a(x + 24, y - nblur));
+                for (int t = -nblur; t <= nblur; t++) {
+                    res0 = _mm256_add_ps(res0, _mm256_load_ps(&a(x + 0, y + t)));
+                    res1 = _mm256_add_ps(res1, _mm256_load_ps(&a(x + 8, y + t)));
+                    res2 = _mm256_add_ps(res2, _mm256_load_ps(&a(x + 16, y + t)));
+                    res3 = _mm256_add_ps(res3, _mm256_load_ps(&a(x + 24, y + t)));
+                }
+                _mm256_stream_ps(&b(x + 0, y), res0);
+                _mm256_stream_ps(&b(x + 8, y), res1);
+                _mm256_stream_ps(&b(x + 16, y), res2);
+                _mm256_stream_ps(&b(x + 24, y), res3);
+            }
+        }
+        benchmark::DoNotOptimize(a);
+    }
+}
+BENCHMARK(BM_y_blur_tiled_only_x_prefetched_streamed_merged_vectorized_interchanged_unrolled_avx_forwarded_manually);
+
+void BM_y_blur_tiled_only_x_prefetched_streamed_merged_vectorized_interchanged_unrolled_avx_forwarded_manually_saturated(benchmark::State &bm) {
+    for (auto _: bm) {
+#pragma omp parallel for collapse(2)
+        for (int x = 0; x < nx; x += 32) {
+            for (int y = 0; y < ny; y++) {
+                _mm_prefetch(&a(x, y + nblur + 40), _MM_HINT_T0);
+                _mm_prefetch(&a(x + 16, y + nblur + 40), _MM_HINT_T0);
+                __m256 res0 = _mm256_load_ps(&a(x + 0, y - nblur));
+                __m256 res1 = _mm256_load_ps(&a(x + 8, y - nblur));
+                __m256 res2 = _mm256_load_ps(&a(x + 16, y - nblur));
+                __m256 res3 = _mm256_load_ps(&a(x + 24, y - nblur));
+                res0 = _mm256_add_ps(res0, _mm256_load_ps(&a(x + 0, y - nblur + 1)));
+                res1 = _mm256_add_ps(res1, _mm256_load_ps(&a(x + 8, y - nblur + 1)));
+                res2 = _mm256_add_ps(res2, _mm256_load_ps(&a(x + 16, y - nblur + 1)));
+                res3 = _mm256_add_ps(res3, _mm256_load_ps(&a(x + 24, y - nblur + 1)));
+                __m256 res4 = _mm256_load_ps(&a(x + 0, y - nblur + 2));
+                __m256 res5 = _mm256_load_ps(&a(x + 8, y - nblur + 2));
+                __m256 res6 = _mm256_load_ps(&a(x + 16, y - nblur + 2));
+                __m256 res7 = _mm256_load_ps(&a(x + 24, y - nblur + 2));
+                for (int t = -nblur + 3; t <= nblur; t += 2) {
+                    res0 = _mm256_add_ps(res0, _mm256_load_ps(&a(x + 0, y + t)));
+                    res1 = _mm256_add_ps(res1, _mm256_load_ps(&a(x + 8, y + t)));
+                    res2 = _mm256_add_ps(res2, _mm256_load_ps(&a(x + 16, y + t)));
+                    res3 = _mm256_add_ps(res3, _mm256_load_ps(&a(x + 24, y + t)));
+                    res4 = _mm256_add_ps(res4, _mm256_load_ps(&a(x + 0, y + t + 1)));
+                    res5 = _mm256_add_ps(res5, _mm256_load_ps(&a(x + 8, y + t + 1)));
+                    res6 = _mm256_add_ps(res6, _mm256_load_ps(&a(x + 16, y + t + 1)));
+                    res7 = _mm256_add_ps(res7, _mm256_load_ps(&a(x + 24, y + t + 1)));
+                }
+                res0 = _mm256_add_ps(res0, res4);
+                res1 = _mm256_add_ps(res1, res5);
+                res2 = _mm256_add_ps(res2, res6);
+                res3 = _mm256_add_ps(res3, res7);
+                _mm256_stream_ps(&b(x + 0, y), res0);
+                _mm256_stream_ps(&b(x + 8, y), res1);
+                _mm256_stream_ps(&b(x + 16, y), res2);
+                _mm256_stream_ps(&b(x + 24, y), res3);
+            }
+        }
+        benchmark::DoNotOptimize(a);
+    }
+}
+BENCHMARK(BM_y_blur_tiled_only_x_prefetched_streamed_merged_vectorized_interchanged_unrolled_avx_forwarded_manually_saturated);
+#endif
+
 BENCHMARK_MAIN();
