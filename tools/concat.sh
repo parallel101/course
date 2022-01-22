ba0020369f2ed352a3ad1be5a390ac587aace6d2
archibate <1931127624@qq.com>
2022-01-22 22:52:56 +0800

    add concat

---
+++ tools/concat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for x in $*; do
+    ffmpeg -i $x.mkv -vcodec copy -acodec copy -vbsf h264_mp4toannexb /tmp/t_$x.ts
+done
+cat /tmp/t_*.ts > /tmp/a_a.ts
+ffmpeg -i /tmp/a_a.ts -acodec copy -vcodec copy -absf aac_adtstoasc /tmp/out.mp4
+rm -f /tmp/a_a.ts /tmp/t_*.ts
