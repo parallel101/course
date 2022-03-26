f2507212930e0814b30c5bc595e4174d7adb6634
archibate <1931127624@qq.com>
2022-03-26 12:07:27 +0800

    cpp raii

---
+++ 12/06_string/11/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <cstdint>
+#include <cstring>
+
+int main() {
+    char str[11];
+    printf("input str:\n");
+    scanf("%10s", str);
+    printf("str: %s\n", str);
+    return 0;
+}
---
+++ 12/06_string/12/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string str;
+    printf("input str:\n");
+    std::cin >> str;
+    printf("str: %s\n", str.c_str());
+    return 0;
+}
