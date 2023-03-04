d8f24c1814572277d049aa9ea2d127a229c4dce1
archibate <1931127624@qq.com>
2023-03-04 16:20:10 +0800

    1

--- stlseries/stl_map/experiment/main.cpp
+++ stlseries/stl_map/experiment/main.cpp
@@ -5,20 +5,20 @@
 using namespace std;
 
 int main() {
-map<string, string> msg = {
-    {"hello", "world"},
-    {"fuck", "rust"},
-};
-print(msg);
-if (msg.count("fuck")) {
-    print("存在fuck，其值为", msg.at("fuck"));
-} else {
-    print("找不到fuck");
-}
-if (msg.count("suck")) {
-    print("存在suck，其值为", msg.at("suck"));
-} else {
-    print("找不到suck");
-}
+    map<string, string> msg = {
+        {"hello", "world"},
+        {"fuck", "rust"},
+    };
+    print(msg);
+    if (msg.count("fuck")) {
+        print("存在fuck，其值为", msg.at("fuck"));
+    } else {
+        print("找不到fuck");
+    }
+    if (msg.count("suck")) {
+        print("存在suck，其值为", msg.at("suck"));
+    } else {
+        print("找不到suck");
+    }
     return 0;
 }
