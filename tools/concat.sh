b2b4163b601653722bc66b6d3b5756031f724136
archibate <1931127624@qq.com>
2024-01-29 14:49:27 +0800

    updconcat

--- tools/concat.sh
+++ tools/concat.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# set -e
+# i=1000
+# for x in $*; do
+#     i=$[$i + 1]
+#     ffmpeg -y -i $x -vcodec copy -acodec copy -vbsf h264_mp4toannexb /tmp/t_$i.ts
+# done
+# cat /tmp/t_*.ts > /tmp/a_a.ts
+# ffmpeg -y -i /tmp/a_a.ts -acodec copy -vcodec copy -absf aac_adtstoasc /tmp/out.mp4
+# rm -f /tmp/a_a.ts /tmp/t_*.ts
+
 set -e
 i=1000
 for x in $*; do
     i=$[$i + 1]
-    ffmpeg -i $x -vcodec copy -acodec copy -vbsf h264_mp4toannexb /tmp/t_$i.ts
+    ffmpeg -y -i $x -vcodec copy -acodec copy /tmp/t_$i.ts
 done
 cat /tmp/t_*.ts > /tmp/a_a.ts
-ffmpeg -y -i /tmp/a_a.ts -acodec copy -vcodec copy -absf aac_adtstoasc /tmp/out.mp4
+ffmpeg -y -i /tmp/a_a.ts -acodec copy -vcodec copy /tmp/out.mp4
 rm -f /tmp/a_a.ts /tmp/t_*.ts
