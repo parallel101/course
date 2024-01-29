df66c13dac3ee13afa52896e07a2244ee0e8556f
archibate <1931127624@qq.com>
2024-01-29 14:48:41 +0800

    upddanmu

--- tools/danmu.py
+++ tools/danmu.py
@@ -5,10 +5,19 @@ import json
 import time
 import os
 
+with open(os.path.expanduser('~/.bilibili-cookies.json'), 'r') as f:
+    cookies = {item['name']: item['value'] for item in json.load(f)}
+
+headers = {
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+}
+
 roomid = 14248205
 url = f'https://api.live.bilibili.com/xlive/web-room/v1/dM/gethistory?roomid={roomid}'
 while True:
-    req = requests.get(url)
+    req = requests.get(url, headers=headers, cookies=cookies)
     data = json.loads(req.text)
     msgs = data['data']['room']
     with open('/tmp/danmu.txt', 'w') as f:
--- tools/qt-danmu.py
+++ tools/qt-danmu.py
@@ -7,15 +7,25 @@ from PyQt5.QtWidgets import *
 
 import threading
 import requests
+import queue
 import json
 import time
 import sys
 import os
 
+with open(os.path.expanduser('~/.bilibili-cookies.json'), 'r') as f:
+    cookies = {item['name']: item['value'] for item in json.load(f)}
+
+headers = {
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+}
+
 def get_messages():
     roomid = 14248205
     url = f'https://api.live.bilibili.com/xlive/web-room/v1/dM/gethistory?roomid={roomid}'
-    req = requests.get(url)
+    req = requests.get(url, headers=headers, cookies=cookies)
     data = json.loads(req.text)
     msgs = data['data']['room']
     res = []
@@ -31,12 +41,35 @@ class MainWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.setWindowOpacity(0.85)
-        self.setWindowFlag(Qt.FramelessWindowHint)
-        self.setWindowFlag(Qt.WindowStaysOnTopHint)
+        icon = QIcon('icon.png')
+        self.setWindowIcon(icon)
+        self.setWindowTitle('弹幕助手')
+        # self.setWindowOpacity(0.95)
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.X11BypassWindowManagerHint)
+        self.setAttribute(Qt.WA_NoSystemBackground)
         self.setAttribute(Qt.WA_TranslucentBackground)
-        self.setWindowTitle('双笙子佯谬的弹幕姬')
-        self.setGeometry(0, 0, 300, 200)
+        self.setAttribute(Qt.WA_TransparentForMouseEvents)
+
+        tray_icon = QSystemTrayIcon(self)
+        tray_icon.setIcon(icon)
+        tray_icon.setToolTip('弹幕助手')
+
+        # Create a menu for the tray icon
+        tray_menu = QMenu(self)
+        action = tray_menu.addAction("显示/隐藏")
+        action.triggered.connect(self.toggle_window)
+        action = tray_menu.addAction("退出")
+        action.triggered.connect(QApplication.quit)
+
+        w, h = 400, 200
+        desktop = QApplication.desktop()
+        bottom_right = desktop.screenGeometry().bottomRight()
+        self.move(bottom_right - QPoint(w, h))
+        self.resize(w, h)
+
+        bg = 0.15
+        fg = 0.55
+        self.setStyleSheet('* {font-size: 18px; color: rgba(255, 255, 255, ' + str(fg) + '); background-color: rgba(0, 0, 0, ' + str(bg) + '); border-radius: 10px; padding: 0px;}')
 
         self.lv = QListView()
         self.lv.setSelectionMode(QAbstractItemView.NoSelection)
@@ -44,18 +77,67 @@ class MainWindow(QWidget):
         self.slm.setStringList(['(没有弹幕)'])
         self.lv.setModel(self.slm)
 
-        self.layout = QVBoxLayout()
-        self.layout.setContentsMargins(0, 0, 0, 0)
-        self.layout.addWidget(self.lv)
-        self.setLayout(self.layout)
+        self.lv.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.lv.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
-        self.update()
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.lv)
 
-    def update(self):
-        threading.Timer(5, self.update).start()
-        msgs = get_messages()
-        # print('update got:', msgs)
-        self.slm.setStringList(msgs)
+        # hlayout = QHBoxLayout()
+        # button = QPushButton()
+        # button.setText('退出')
+        # button.clicked.connect(QApplication.quit)
+        # hlayout.addWidget(button)
+        #
+        # button = QPushButton()
+        # button.setText('退出')
+        # button.clicked.connect(QApplication.quit)
+        # hlayout.addWidget(button)
+        #
+        # layout.addLayout(hlayout)
+
+        self.setLayout(layout)
+
+        self.queue = queue.Queue(maxsize=1)
+
+        self.update_messages()
+
+        timer = QTimer(self)
+        timer.timeout.connect(self.update_messages)
+        timer.start(1000)
+        timer.setSingleShot(False)
+        
+        threading.Thread(target=self.message_worker, daemon=True).start()
+
+    def toggle_window(self):
+        if self.isVisible():
+            self.hide()
+        else:
+            self.show()
+
+    def message_worker(self):
+        while True:
+            try:
+                msgs = get_messages()
+            except:
+                import traceback
+                traceback.print_exc()
+                continue
+            try:
+                self.queue.put(msgs, block=False)
+            except queue.Full:
+                pass
+            time.sleep(5)
+
+    def update_messages(self):
+        try:
+            msgs = self.queue.get(block=False)
+        except queue.Empty:
+            return
+        if self.slm.stringList() != msgs:
+            self.slm.setStringList(msgs)
+            self.lv.scrollToBottom()
 
 
 def main():
