67997ddbd0a61d516bd3151f9d087745b0773f58
archibate <1931127624@qq.com>
2024-06-16 13:16:20 +0800

    new tests

--- slides/bench/chart.py
+++ slides/bench/chart.py
@@ -1,13 +1,16 @@
 import csv
 import os
 import re
+import sys
 import matplotlib.pyplot as plt
 
-if not os.path.exists('result.csv'):
-    os.system('build/main --benchmark_format=csv > result.csv')
+key = sys.argv[1]
+
+if not os.path.exists(key + '.csv'):
+    os.system('build/main --benchmark_filter=' + key + ' --benchmark_format=csv | tee ' + key + '.csv')
 
 # read result.csv and plot it
-with open('result.csv', 'r') as csv_file:
+with open(key + '.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
     next(csv_reader) # skip header row
     x = []
@@ -15,15 +18,18 @@ with open('result.csv', 'r') as csv_file:
     for row in csv_reader:
         name = row[0]
         # name format: BM_latency/offset:58/min_time:0.050, we need the offset only
-        match = re.search(r'/offset:(\d+)', name)
+        match = re.search(key + r'/offset:(\d+)', name)
         if match:
             offset = int(match.group(1))
             x.append(offset)
             cpu_time = float(row[3])
             y.append(cpu_time)
 
-plt.bar(x, y)
+bar = plt.bar(x, y)
+for i in range(len(bar)):
+    if i % 8 != 0:
+        bar[i].set_color('#1fb477')
 plt.xlabel('Offset (bytes)')
 plt.ylabel('CPU Time (ns)')
-plt.title('Benchmark Result')
+plt.title(key)
 plt.show()
--- slides/bench/test.cpp
+++ slides/bench/test.cpp
@@ -1,7 +1,7 @@
 #include <benchmark/benchmark.h>
 
 void BM_latency(benchmark::State &state) {
-    int offset = state.range(0);
+    size_t offset = state.range(0);
     const size_t repeats = 100;
     alignas(64) static char buf[256];
     uintptr_t &tmp = *new (buf + offset) uintptr_t;
@@ -13,7 +13,32 @@ void BM_latency(benchmark::State &state) {
             rax = *(uintptr_t *)rax; // mov rax, [rax]
         }
     }
-    tmp = rax;
-    benchmark::DoNotOptimize(tmp);
+    benchmark::DoNotOptimize(rax);
 }
 BENCHMARK(BM_latency)->MinTime(0.05)->ArgName("offset")->DenseRange(0, 80);
+
+struct alignas(8) A {
+    uint32_t a;
+    uint32_t b;
+};
+
+void BM_struct(benchmark::State &state) {
+    size_t offset = state.range(0);
+    const size_t repeats = 100;
+    alignas(64) static char buf[256];
+    A &tmp = *new (buf + offset) A;
+    tmp.a = 0;
+    tmp.b = 0;
+    benchmark::DoNotOptimize(tmp);
+    uintptr_t rax = (uintptr_t)&tmp;
+    for (auto _: state) {
+        #pragma GCC unroll repeats
+        for (size_t i = 0; i < repeats; ++i) {
+            // movzx rbx, dword [rax]; add rax, rbx; movzx rbx, dword [rax + 4]; add rax, rbx
+            rax += ((A *)rax)->a;
+            rax += ((A *)rax)->b;
+        }
+    }
+    benchmark::DoNotOptimize(rax);
+}
+BENCHMARK(BM_struct)->MinTime(0.05)->ArgName("offset")->DenseRange(0, 80);
