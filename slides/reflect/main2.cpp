ed251a3e4bb7b8a614109fe1aa29bf2effce5c88
archibate <1931127624@qq.com>
2024-06-28 19:00:38 +0800

    fincpp14

--- slides/reflect/main2.cpp
+++ slides/reflect/main2.cpp
@@ -3,7 +3,6 @@
 #include <memory>
 #include <sstream>
 #include <json/json.h>
-// #include <pybind11/pybind11.h>
 #include "reflect.hpp"
 
 
@@ -38,7 +37,7 @@ struct Student {
 };
 
 
-std::string toString(Json::Value root) {
+std::string jsonToStr(Json::Value root) {
     Json::StreamWriterBuilder builder;
     builder["indentation"] = "";
     std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
@@ -47,47 +46,40 @@ std::string toString(Json::Value root) {
     return os.str();
 }
 
-Json::Value fromString(std::string const &json) {
+Json::Value strToJson(std::string const &json) {
     Json::Value root;
     Json::Reader reader;
     reader.parse(json, root);
     return root;
 }
 
-template <class T> requires (!reflect_trait<T>::has_member())
-Json::Value serialize(T const &object) {
+template <class T, std::enable_if_t<!reflect::has_member<T>(), int> = 0>
+Json::Value objToJson(T const &object) {
     return object;
 }
 
-template <class T> requires (reflect_trait<T>::has_member())
-Json::Value serialize(T const &object) {
+template <class T, std::enable_if_t<reflect::has_member<T>(), int> = 0>
+Json::Value objToJson(T const &object) {
     Json::Value root;
-    foreach_member(object, [&](const char *key, auto &value) {
-        root[key] = serialize(value);
+    reflect::foreach_member(object, [&](const char *key, auto &value) {
+        root[key] = objToJson(value);
     });
     return root;
 }
 
-template <class T> requires (!reflect_trait<T>::has_member())
-T deserialize(Json::Value const &root) {
+template <class T, std::enable_if_t<!reflect::has_member<T>(), int> = 0>
+T jsonToObj(Json::Value const &root) {
     return root.as<T>();
 }
 
-template <class T> requires (reflect_trait<T>::has_member())
-T deserialize(Json::Value const &root) {
+template <class T, std::enable_if_t<reflect::has_member<T>(), int> = 0>
+T jsonToObj(Json::Value const &root) {
     T object;
-    foreach_member(object, [&](const char *key, auto &value) {
-        value = deserialize<std::decay_t<decltype(value)>>(root[key]);
+    reflect::foreach_member(object, [&](const char *key, auto &value) {
+        value = jsonToObj<std::decay_t<decltype(value)>>(root[key]);
     });
     return object;
 }
-
-// PYBIND11_MODULE(pyreflect, m) {
-//     pybind11::class_<Address> b(m, "Address");
-//     reflect_trait<Address>::foreach_member_ptr([&] (const char *name, auto member) {
-//         b.def(name, member);
-//     });
-// }
 
 int main() {
     Student stu = {
@@ -99,9 +91,9 @@ int main() {
             .city = "Shanghai",
         }
     };
-    std::string bin = toString(serialize(stu));
+    std::string bin = jsonToStr(objToJson(stu));
     std::cout << bin << '\n';
-    auto stuDes = deserialize<Student>(fromString(bin));
+    auto stuDes = jsonToObj<Student>(strToJson(bin));
     std::cout << stuDes.name << '\n';
     std::cout << stuDes.age << '\n';
     std::cout << stuDes.addr.country << '\n';
@@ -109,3 +101,11 @@ int main() {
     std::cout << stuDes.addr.city << '\n';
     return 0;
 }
+
+// #include <pybind11/pybind11.h>
+// PYBIND11_MODULE(pyreflect, m) {
+//     pybind11::class_<Address> b(m, "Address");
+//     foreach_member_ptr<Address>([&] (const char *name, auto member) {
+//         b.def(name, member);
+//     });
+// }
